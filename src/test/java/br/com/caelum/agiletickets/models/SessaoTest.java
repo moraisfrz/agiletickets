8ac1eb12aa2195721461c256a35f37b79db33d3f
Leonardo Lima <ltorresdl@gmail.com>
2011-08-18 12:50:33 -0400

    refatoracao de testes da sessao

--- src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
+++ src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
@@ -3,38 +3,35 @@ package br.com.caelum.agiletickets.models;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class SessaoTest {
+class SessaoTest {
 
-
+	private Sessao sessao;
+	
+	public void setup(){
+		this.sessao = new Sessao();
+	}
+	
 	@Test
 	public void deveVender1ingressoSeHa2vagas() throws Exception {
-		Sessao sessao = new Sessao();
-        sessao.setTotalIngressos(2);
-
+ 	    sessao.setTotalIngressos(2);
         Assert.assertTrue(sessao.podeReservar(1));
 	}
 	
 	@Test
 	public void deveVender1ingressoSeHa1vaga() throws Exception {
-		Sessao sessao = new Sessao();
-        sessao.setTotalIngressos(1);
-
+		sessao.setTotalIngressos(1);
         Assert.assertTrue(sessao.podeReservar(1));
 	}
 
 	@Test
 	public void naoDeveVender3ingressoSeHa2vagas() throws Exception {
-		Sessao sessao = new Sessao();
 		sessao.setTotalIngressos(2);
-
 		Assert.assertFalse(sessao.podeReservar(3));
 	}
 
 	@Test
 	public void reservarIngressosDeveDiminuirONumeroDeIngressosDisponiveis() throws Exception {
-		Sessao sessao = new Sessao();
 		sessao.setTotalIngressos(5);
-
 		sessao.reserva(3);
 		Assert.assertEquals(2, sessao.getIngressosDisponiveis().intValue());
 	}
